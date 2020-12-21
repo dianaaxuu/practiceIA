79c7b54a691ceef75b44847f1c304602b779cadf
Clearlove923 <75820637+Clearlove923@users.noreply.github.com>
2020-12-21 14:35:05 +0800

    Add files via upload

---
+++ main code.py
@@ -0,0 +1,6 @@
+start=input("Hello, Mr. Kauk! Welcome to the movie recommandation program. Type '1' to start, or type any other to quit. ")
+if start == '1':
+    #genre
+    print("Here is the list of genre: ")
+    print(list_genre)
+    print("Please type your preferable genre code: ")
